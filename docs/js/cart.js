15a83e2c4c8029b916cbc993c0821b5e7c3afba6
dmitrolov <dmitrolov@gmail.com>
2020-10-14 00:48:47 +0300

    ghp remove

--- docs/js/cart.js
+++
@@ -1,9 +0,0 @@
-function addToCart(i) {
-	if ($(`#Cart-${i}`)[0]) {$(`#Cart-${i}`).remove();}
-	else {
-		var lotsInCart = parseInt($('#lotsInCartCount')[0].innerHTML);
-		$('#lotsInCartCount')[0].innerHTML = ++lotsInCart;
-	}
-	makeABid(i);
-	appendLot(lots, i, i+1, ".cartLotsContainer", "Cart");
-}
